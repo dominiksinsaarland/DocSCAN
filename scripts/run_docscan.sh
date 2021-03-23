f797d4e2ca1cbbf8d4d5eb8c7e7026fa46589f2f
dominiksinsaarland <dominik.stammbach@bluewin.ch>
2021-03-23 19:12:00 +0100

    finished script for new data

--- scripts/run_docscan.sh
+++ scripts/run_docscan.sh
@@ -2,24 +2,22 @@ path=$1
 num_clusters=$2
 
 # compute embeddings
-
-
-#python src/compute_sbert_embeddings.py --infile $path/train.jsonl --outfile $path/train_embedded.pkl
-#python src/compute_sbert_embeddings.py --infile $path/test.jsonl --outfile $path/test_embedded.pkl
+python src/compute_sbert_embeddings.py --infile $path/train.jsonl --outfile $path/train_embedded.pkl
+python src/compute_sbert_embeddings.py --infile $path/test.jsonl --outfile $path/test_embedded.pkl
 
 # baselines
 # kmeans tf-idf baseline
+python src/kmeans.py --path $path --features tfidf
 # kmeans sbert baseline
-
-#python src/kmeans.py --path $path --features tfidf
-#python src/kmeans.py --path $path --features sbert
-
-
+python src/kmeans.py --path $path --features sbert
 
 # retrieve neighbors
-# python src/find_nearest_neighbours.py --path $path
+ python src/find_nearest_neighbours.py --path $path
 
+# train DOCSCAN
 python src/scan_classification_layer.py --path $path --num_clusters $num_clusters
+
+# print results
 
 printf "\nkmeans with tf-idf features: mean accuracy over 5 runs\n"
 cat $path/mean_kmeans_tfidf.txt
