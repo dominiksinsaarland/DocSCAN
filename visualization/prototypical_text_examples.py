1a8288ca55fd3fce136ea773862b8d0ff349993f
dominiksinsaarland <dominik.stammbach@bluewin.ch>
2021-06-08 11:58:30 +0200

    added visualization

---
+++ visualization/prototypical_text_examples.py
@@ -0,0 +1,63 @@
+import os
+import pandas as pd
+import numpy as np
+import argparse
+import json
+
+def load_clusters(fn):
+	with open(fn) as f:
+		clusters = [i.strip() for i in f]
+	return clusters
+
+def load_probabilities(fn):
+	with open(fn) as f:
+		probabilities = [list(map(float, i.strip().split())) for i in f]
+	return probabilities
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--path", default="20newsgroup", type=str, help="", required=True)
+	parser.add_argument("--outfile", default="prototypical_examples_by_clusters", type=str, help="")
+	args = parser.parse_args()
+
+
+	if os.path.exists(os.path.join(args.path, "test_embedded.pkl")):
+		filename = os.path.join(args.path, "test_embedded.pkl")
+	else:
+		filename = os.path.join(args.path, "train_embedded.pkl")
+
+	if os.path.exists(os.path.join(args.path, "label2id.json")):
+		with open(os.path.join(args.path, "label2id.json")) as f:		
+			label2id = json.load(f)
+	else:
+		label2id = {}
+
+
+	df = pd.read_pickle(filename)
+	clusters = load_clusters(os.path.join(args.path, "predictions.txt"))
+	probabilities = load_probabilities(os.path.join(args.path, "probabilities.txt"))
+	df["clusters"] = clusters
+	df["probabilities"] = probabilities
+
+	with open(os.path.join(args.path, args.outfile), "w") as outfile:
+		for topic in np.unique(clusters):
+			df_topic = df[df["clusters"] == topic]
+			if label2id:
+				probabilites = [i[label2id[topic]] for i in df["probabilities"]]
+			else:
+				probabilites = [i[label2id[int(topic)]] for i in df["probabilities"]]
+			indices = np.argsort(probabilites)[-10:]
+			for i in indices:
+				#print (probabilities[i], "--", df.iloc[i]["sentence"])
+				outfile.write(topic + "\t" + df.iloc[i]["sentence"] + "\n")
+			#input("")
+
+
+			"""
+			most_probable_topics = np.argsort(df_topic["probabilities"])[-5:]
+			for index in most_probable_topics:
+				print (topic, "--", 
+				sentence = df_topic.iloc[index]["sentence"]
+				outfile.write(topic + "\t" + sentence + "\n")
+			"""
+
---
+++ visualization/word_clouds.py
@@ -0,0 +1,88 @@
+import os
+import argparse
+import pandas as pd
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from spacy.lang.en import English
+from collections import Counter
+import numpy as np
+from random import randint
+
+def load_clusters(fn):
+	print (fn)
+	with open(fn) as f:
+		clusters = [i.strip() for i in f]
+	print (len(clusters))
+	return clusters
+
+def generate_word_clouds(topic, df_topic, nlp, outpath):
+	word_counts = Counter()
+	for sent in df_topic["sentence"]:
+		word_counts.update([tok.lemma_.lower() for tok in nlp(sent)])
+
+	maincol = randint(0,360)
+	def colorfunc(word=None, font_size=None, 
+		  position=None, orientation=None, 
+		  font_path=None, random_state=None):   
+		color = randint(maincol-10, maincol+10)
+		if color < 0:
+			color = 360 + color
+		return "hsl(%d, %d%%, %d%%)" % (color,randint(65, 75)+font_size / 7, randint(35, 45)-font_size / 10)   
+
+	wordcloud = WordCloud(background_color="white", 
+		          ranks_only=False, 
+		          max_font_size=120,
+		          color_func=colorfunc,
+		          height=600,width=800).generate_from_frequencies(word_counts)
+	print (word_counts.most_common(n=25))
+
+	if topic.isdigit():
+		# if we don't have topic label, but just e.g. cluster "86", we take the first five words as a description		
+		save_filename = topic + "_" + "_".join([i[0] for i in word_counts.most_common(n=5)])
+	else:
+		save_filename = topic
+	plt.clf()
+	plt.imshow(wordcloud,interpolation="bilinear")
+	plt.axis("off")
+	#plt.show()
+	plt.savefig(os.path.join(outpath, save_filename))
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--path", default="20newsgroup", type=str, help="", required=True)
+	parser.add_argument("--outpath", default="wordclouds", type=str, help="")
+
+	args = parser.parse_args()
+
+	outpath = os.path.join(args.path, args.outpath)
+	os.makedirs(outpath, exist_ok=True)
+
+
+	nlp = English()
+	tokenizer = nlp.Defaults.create_tokenizer(nlp)
+	nlp.add_pipe(nlp.create_pipe('sentencizer'))
+
+
+	if os.path.exists(os.path.join(args.path, "test_embedded.pkl")):
+		filename = os.path.join(args.path, "test_embedded.pkl")
+	else:
+		filename = os.path.join(args.path, "train_embedded.pkl")
+
+	df = pd.read_pickle(filename)
+	clusters = load_clusters(os.path.join(args.path, "predictions.txt"))
+	df["clusters"] = clusters
+	for topic in np.unique(clusters):
+		df_topic = df[df["clusters"] == topic]
+		print (topic, len(df_topic))
+		generate_word_clouds(topic, df_topic, nlp, outpath)
+	"""
+	genearte_word_clouds
+	# right
+	print (df.columns)
+	for i,j in zip(clusters, df["sentence"]):
+		print (i, "--",j)
+		input("")
+
+	#preprocess(args.path, args.infile)
+
+	"""
