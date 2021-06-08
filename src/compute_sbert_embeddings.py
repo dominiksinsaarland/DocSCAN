e0bff48730b0c7ba204ed3c19224cc06020b8794
dominiksinsaarland <dominik.stammbach@bluewin.ch>
2021-06-08 18:08:15 +0200

    added max_length

--- src/compute_sbert_embeddings.py
+++ src/compute_sbert_embeddings.py
@@ -21,6 +21,8 @@ if __name__ == "__main__":
 	parser.add_argument("--model", default="", type=str, help="")
 	parser.add_argument("--infile", default=None, type=str, help="", required=True)
 	parser.add_argument("--outfile", default=None, type=str, help="", required=True)
+	parser.add_argument("--max_seq_length", default=128, type=int, help="", required=True)
+
 	args = parser.parse_args()
 	print (args.infile)
 	print (args.outfile)
@@ -30,6 +32,7 @@ if __name__ == "__main__":
 	sentences, labels = load_data(args.infile)
 	#embedder = SentenceTransformer(args.model, cache_dir="/cluster/work/lawecon/Work/dominik/transformer_models")
 	embedder = SentenceTransformer(args.model)
+	embedder.max_seq_length = args.max_seq_length
 	corpus_embeddings = embedder.encode(sentences, batch_size=32, show_progress_bar=True)
 	df = pd.DataFrame(list(zip(sentences, labels, corpus_embeddings)), columns=["sentence", "label", "embeddings"])
 	df.to_pickle(args.outfile)
